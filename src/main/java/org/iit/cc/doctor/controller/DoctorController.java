adfb406542cd84aba1474b59ab1e0c72641c1ec9
Dhananjana Milinda <dhananjana.20240366@iit.ac.lk>
2024-12-18 16:44:58 +0530

    [IIT] changed main api

--- src/main/java/org/iit/cc/doctor/controller/DoctorController.java
+++ src/main/java/org/iit/cc/doctor/controller/DoctorController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/doctors")
+@RequestMapping("/api/doctors")
 public class DoctorController {
 
     @Autowired
